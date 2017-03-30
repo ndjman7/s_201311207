2ce2060eac1d5fdf17d3688531f5dc77d3493ed6
ndjman7 <ndjman7@gmail.com>
2017-03-30 20:25:38 +0900

    Add quiz1

---
+++ src/ds_web_quiz1.py
@@ -0,0 +1,9 @@
+import re
+import requests
+response = requests.get('http://python.org/')
+html = response.text
+p = re.compile('href="(http://.*?)"')
+nodes = p.findall(html)
+print "http url의 개수는",len(nodes),'개'
+for index, node in enumerate(nodes):
+    print index+1,'번',node
